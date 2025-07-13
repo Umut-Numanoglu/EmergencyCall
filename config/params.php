0aec7dc01e8c3c71296e3447883ee4211e55d14f
Umut Numanoglu <umut.numanoglu@gmail.com>
2025-07-13 20:57:18 +0200

    forgot

--- config/params.php
+++ config/params.php
@@ -3,4 +3,5 @@ return [
     'adminEmail' => 'admin@numanoglu.org',
     'senderEmail' => 'noreply@numanoglu.org',
     'senderName' => 'Emergency Call System',
+    'frontendPath' => dirname(dirname(__DIR__)) . '/frontend',
 ]; 
